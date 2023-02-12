b6428c84d06f6f10c846d4843abf2ebfc4d80ee0
MajidCamp <majid.edus@gmail.com>
2023-02-12 20:24:17 +0200

    first commit

--- app/src/main/java/com/example/ktorsampleapp/model/Routes.kt
+++ app/src/main/java/com/example/ktorsampleapp/model/Routes.kt
@@ -1,11 +1,10 @@
 package com.example.ktorexamplelab.model
 
-/**
- * Created by Taha Ben Ashur (https://github.com/tahaak67) on 08,Feb,2023
- */
-object Routes {
 
-    private const val BASE_URL = "https://freefakeapi.io/api"
-    const val POSTS = "$BASE_URL/posts"
+object Routes {
+    private const val Base_MEME = " https://meme-api.com/gimme"
+    const val WHOLESOME = "$Base_MEME/wholesome/6"
+    const val DARKSOULS = "$Base_MEME/darksouls/6"
+    const val ANIMAL = "$Base_MEME/FunnyAnimals/6"
 
 }
